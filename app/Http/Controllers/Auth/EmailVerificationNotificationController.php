8e866efa68a80651d957430bb42103bfc14c9fbd
ArmandCortz <armand1515.lc@gmail.com>
2023-03-01 21:45:32 -0600

    version 1.0

--- app/Http/Controllers/Auth/EmailVerificationNotificationController.php
+++ app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -4,26 +4,20 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
-<<<<<<< HEAD
 use Illuminate\Http\RedirectResponse;
-=======
->>>>>>> 9014123e383dd9fec56bbc4b8d1e29c9644bcb13
 use Illuminate\Http\Request;
 
 class EmailVerificationNotificationController extends Controller
 {
     /**
      * Send a new email verification notification.
-<<<<<<< HEAD
      */
     public function store(Request $request): RedirectResponse
-=======
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\RedirectResponse
      */
     public function store(Request $request)
->>>>>>> 9014123e383dd9fec56bbc4b8d1e29c9644bcb13
     {
         if ($request->user()->hasVerifiedEmail()) {
             return redirect()->intended(RouteServiceProvider::HOME);
--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -6,32 +6,23 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
-<<<<<<< HEAD
 use Illuminate\Http\RedirectResponse;
-=======
->>>>>>> 9014123e383dd9fec56bbc4b8d1e29c9644bcb13
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
-<<<<<<< HEAD
 use Illuminate\View\View;
-=======
->>>>>>> 9014123e383dd9fec56bbc4b8d1e29c9644bcb13
 
 class RegisteredUserController extends Controller
 {
     /**
      * Display the registration view.
-<<<<<<< HEAD
      */
     public function create(): View
-=======
      *
      * @return \Illuminate\View\View
      */
     public function create()
->>>>>>> 9014123e383dd9fec56bbc4b8d1e29c9644bcb13
     {
         return view('auth.register');
     }
@@ -39,7 +30,6 @@ class RegisteredUserController extends Controller
     /**
      * Handle an incoming registration request.
      *
-<<<<<<< HEAD
      * @throws \Illuminate\Validation\ValidationException
      */
     public function store(Request $request): RedirectResponse
@@ -47,7 +37,6 @@ class RegisteredUserController extends Controller
         $request->validate([
             'nombres' => ['required', 'string', 'max:255'],
             'apellidos' => ['required', 'string', 'max:255'],
-=======
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\RedirectResponse
      *
@@ -58,19 +47,15 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
->>>>>>> 9014123e383dd9fec56bbc4b8d1e29c9644bcb13
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
-<<<<<<< HEAD
             'nombres' => $request->nombres,
             'apellidos' => $request->apellidos,
             'id' => $request->id,
-=======
             'name' => $request->name,
             'email' => $request->email,
->>>>>>> 9014123e383dd9fec56bbc4b8d1e29c9644bcb13
             'password' => Hash::make($request->password),
         ]);
 
@@ -80,8 +65,5 @@ class RegisteredUserController extends Controller
 
         return redirect(RouteServiceProvider::HOME);
     }
-<<<<<<< HEAD
 }
-=======
 }
->>>>>>> 9014123e383dd9fec56bbc4b8d1e29c9644bcb13
